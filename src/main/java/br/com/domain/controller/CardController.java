e3331c68f16cf827e74dee02cdefb7bb2da6a4d6
JonathAM20 <jonathan_msi@live.com>
2021-08-18 17:42:29 -0400

    Implementacao de Documentacao com Swagger Parte 2

--- src/main/java/br/com/domain/controller/CardController.java
+++ src/main/java/br/com/domain/controller/CardController.java
@@ -1,7 +1,11 @@
 package br.com.domain.controller;
 
 import br.com.domain.domain.Card;
+import br.com.domain.domain.ErrorDetail;
 import br.com.domain.service.CardService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -9,38 +13,76 @@ import javax.validation.Valid;
 import java.util.List;
 
 @RestController
-@RequestMapping(value = "/card")
+@RequestMapping(value = "/card", headers = {"Authorization=Bearer {token}"})
 public class CardController {
 
     @Autowired
     private CardService service;
 
-    @GetMapping
+    @GetMapping(produces = "application/json")
+    @ApiOperation(value = "List datas about overall cards")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Success", response = Card[].class),
+            @ApiResponse(code = 403, message = "Forbidden", response = ErrorDetail.class),
+    })
     public Iterable<Card> findAll(){
         return service.findAll();
     }
 
-    @GetMapping("{id}")
+    @GetMapping(value = "{id}", params = "id=1", produces = "application/json")
+    @ApiOperation(value = "List datas about an specific card")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Success"),
+            @ApiResponse(code = 403, message = "Forbidden", response = ErrorDetail.class),
+            @ApiResponse(code = 404, message = "Bad Request", response = ErrorDetail.class),
+    })
     public Card findById(@PathVariable("id") Long id){
         return service.findById(id);
     }
 
-    @GetMapping("/customer/{id}")
+    //@GetMapping("/customer/{id}")
+    @GetMapping(value = "/customer/{id}", params = "id=1", produces = "application/json")
+    @ApiOperation(value = "List cards of an specific customer")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Success"),
+            @ApiResponse(code = 403, message = "Forbidden", response = ErrorDetail.class),
+            @ApiResponse(code = 404, message = "Bad Request", response = ErrorDetail.class),
+    })
     public List<Card> findByCustomerId(@PathVariable("id") Long id){
         return service.findByCustomerId(id);
     }
 
-    @PostMapping
+    @PostMapping(consumes = "application/json", produces = "application/json")
+    @ApiOperation(value = "Save datas of an specific card")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Success"),
+            @ApiResponse(code = 403, message = "Forbidden", response = ErrorDetail.class),
+            @ApiResponse(code = 404, message = "Bad Request", response = ErrorDetail.class),
+            @ApiResponse(code = 409, message = "Conflict", response = ErrorDetail.class),
+    })
     public Card save(@Valid @RequestBody Card card){
         return service.save(card);
     }
 
-    @PutMapping
+    @PutMapping(consumes = "application/json", produces = "application/json")
+    @ApiOperation(value = "Update datas of an specific card")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Success"),
+            @ApiResponse(code = 403, message = "Forbidden", response = ErrorDetail.class),
+            @ApiResponse(code = 404, message = "Bad Request", response = ErrorDetail.class),
+            @ApiResponse(code = 409, message = "Conflict", response = ErrorDetail.class),
+    })
     public Card update(@Valid @RequestBody Card card){
         return service.update(card);
     }
 
-    @DeleteMapping(path ={"/{id}"})
+    @DeleteMapping(path ={"/{id}"}, params = "id=1", produces = "application/json")
+    @ApiOperation(value = "Delete datas of an specific customer")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Success"),
+            @ApiResponse(code = 403, message = "Forbidden", response = ErrorDetail.class),
+            @ApiResponse(code = 404, message = "Bad Request", response = ErrorDetail.class),
+    })
     public Card delete(@PathVariable long id){
         return service.delete(id);
     }
--- src/main/java/br/com/domain/domain/Card.java
+++ src/main/java/br/com/domain/domain/Card.java
@@ -1,5 +1,6 @@
 package br.com.domain.domain;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -18,11 +19,21 @@ public class Card {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id_card")
+    @ApiModelProperty(
+            value = "Identification of the Card",
+            name = "id",
+            dataType = "Long",
+            example = "1")
     private Long id;
 
     @NotNull
     @Size(min = 16, max = 16)
     @Column(name = "cd_pan", nullable = false, length = 16)
+    @ApiModelProperty(
+            value = "Specific code card",
+            name = "id",
+            dataType = "Long",
+            example = "1")
     private String pan;
 
     @NotNull
